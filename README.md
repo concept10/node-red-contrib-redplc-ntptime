90afc300c8f1a0fb7594fe41ac61240bb62461e3
Ocean (Derya) <iot.redplc@gmail.com>
2021-03-20 17:58:13 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,31 @@
+# node-red-contrib-redplc-ntptime
+
+[![platform](https://img.shields.io/badge/platform-Node--RED-red)](https://nodered.org)
+![NPM version](https://badge.fury.io/js/node-red-contrib-redplc-ntptime.svg)
+![NPM](https://img.shields.io/npm/l/node-red-contrib-redplc-ntptime)
+[![Donate](https://img.shields.io/badge/Donate-PayPal-green.svg)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=ZDRCZBQFWV3A6)
+
+## Node Features
+- Reads time from NTP server and store in Node-Red global variable as array<br>
+- On read failure reads system time<br>
+- Update on cycle time interval<br>
+
+
+## Install
+
+Install with Node-Red Palette Manager or npm command:
+```
+npm install node-red-contrib-redplc-ntptime
+```
+
+### Time/Date Elements (Variable IA):
+|Element|Value|Array-Index|
+|---|---|---|
+|Seconds|0 .. 59|0|
+|Minutes|0 .. 59|1|
+|Hours|0 .. 12/23|2|
+|AM/PM|0=AM, 1=PM|3|
+|Weekday|0 .. 7, 0=Sunday|4|
+|Day|1 .. 31|5|
+|Month|1 .. 12|6|
+|Year|YYYY|7|
---
+++ nodes/ntptime.js
@@ -0,0 +1,121 @@
+/**
+ * Copyright 2021 Ocean (iot.redplc@gmail.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+module.exports = function(RED) {
+	"use strict";
+
+    const dgram = require('dgram');
+	const syslib = require('./lib/syslib.js');
+
+	RED.nodes.registerType("ntptime", function(n) {
+		var node = this;
+		RED.nodes.createNode(node, n);
+
+		node.tagnameai = "IA" + n.addressai;
+		node.tupdate = n.tupdate;
+        node.ntpserver = n.ntpserver;
+		node.ampm = n.ampm;
+		node.systime = n.systime;
+		node.iserror = false;
+		node.setai = false;
+		node.onWork = false;
+		node.ntpclient = dgram.createSocket("udp4");
+		node.ntpData = new Buffer.alloc(48);
+		node.ntpData[0] = 0x1B;
+
+		node.store = node.context().global;
+
+		function setDateTime(dt) {
+			var hr = dt.getHours();
+			var ap = (hr >= 12) ? 1 : 0;
+			var hr = (node.ampm) ? hr % 12 : hr;
+			node.store.set(node.tagnameai, [dt.getSeconds(), dt.getMinutes(), hr, ap, dt.getDay(), dt.getDate(), dt.getMonth() + 1, dt.getFullYear()]);
+		}
+
+		node.ntpclient.on('message', function (data) {
+			clearTimeout(node.id_sendtimeout);
+
+			var part1 = 0, part2 = 0;
+
+			for (var i = 0; i <= 3; i++) {
+				part1 = 256 * part1 + data[40 + i];
+			}
+
+			for (i = 4; i <= 7; i++) {
+				part2 = 256 * part2 + data[40 + i];
+			}
+
+			var date = new Date("Jan 01 1900 GMT");
+
+			date.setUTCMilliseconds(date.getUTCMilliseconds() + (part1 * 1000 + (part2 * 1000) / 0x100000000));
+			setDateTime(date);
+			syslib.setStatus(node, node.tagnameai);
+			node.onWork = false;
+		});
+
+		function updateDateTime()
+		{
+			if (node.onWork || node.iserror)
+				return;
+			
+			node.onWork = true;
+
+            for (var i = 1; i < 48; i++)
+                node.ntpData[i] = 0;
+
+            node.ntpclient.send(node.ntpData, 0, node.ntpData.length, 123, node.ntpserver, function (err) {
+                if (err) {
+					if (node.systime) {
+						setDateTime(new Date());
+						syslib.setStatus(node, node.tagnameai);
+					}
+					else
+						syslib.outError(node, "server failure", "server failure");
+
+					node.onWork = false;
+					return;
+				}
+
+                node.id_sendtimeout = setTimeout(function () {
+					if (node.systime) {
+						setDateTime(new Date());
+						syslib.setStatus(node, node.tagnameai);
+					}
+					node.onWork = false;
+	            }, 3000);
+            });
+		}
+
+		node.statustxt = "";
+
+		if (node.store.keys().find(key => key === node.tagnameai) !== undefined)
+			node.iserror = syslib.outError(node, "duplicate " + node.tagnameai, "duplicate address " + node.tagnameai);
+		else {
+			setDateTime(new Date());
+			node.setai = true;
+		}
+
+		node.id_loop = setInterval(updateDateTime, node.tupdate);
+
+		node.on('close', function () {
+			clearTimeout(node.id_sendtimeout);
+			clearInterval(node.id_loop);
+			node.ntpclient.close();
+			if (node.setai)
+				node.store.set(node.tagnameai, undefined);
+		});
+	});
+}
